5da736792b922cc73e6d1215fb0fb5c1aabeff1e
Roberto Russo <roberto.russo@aulab.it>
2024-05-09 11:28:07 +0200

    creazione del metoto costruttore nella classe Person

--- spec_java/021_java/src/App.java
+++ spec_java/021_java/src/App.java
@@ -5,11 +5,20 @@ public class App {
         // assegnazione dei valori degli attributi name e surname dell'oggetto p
         p.name = "Robbie";
         p.surname = "Russian";
+
+        // creazione di un oggetto di tipo Person
+        Person laraC = new Person();
+        //assegnazione dei valori degli attributi name e surname dell'oggetto laraC
+        laraC.name = "Lara";
+        laraC.surname = "Croft";
+
         // chiamata al metodo info() passando l'oggetto p come argomento della funzione
         info(p);
+        // chiamata al metodo info() passando l'oggetto laraC come argomento della funzione
+        info(laraC);
     }
     //definizione del metodo info() che riceveun oggetto di tipo Person come parametro
-    public static void info(Person p){
-        System.out.println("Ciao il mio nome è "+ p.name + " ed il mio cognome è " + p.surname);
-    }
+    // public static void info(Person person){
+    //     System.out.println("Ciao il mio nome è "+ person.name + " ed il mio cognome è " + person.surname);
+    // }
 }
--- spec_java/021_java/src/Person.java
+++ spec_java/021_java/src/Person.java
@@ -2,5 +2,17 @@ public class Person {
     //! attributi
     String name;
     String surname;
+    
+    //! costruttore
+    
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+    
     //! metodi
+    public static void info(String name, String surname){
+        System.out.println("Ciao il mio nome è "+ name + " ed il mio cognome è " + surname);
+    }
+
 }
