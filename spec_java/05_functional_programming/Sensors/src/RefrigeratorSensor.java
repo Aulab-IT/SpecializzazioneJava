9a8d4adf905f76c4865d4ff0f30b871ba1c56392
Alessandro Leo <alessandro.leo@aulab.it>
2024-06-05 12:15:16 +0200

    Functional completo

---
+++ spec_java/05_functional_programming/Sensors/src/RefrigeratorSensor.java
@@ -0,0 +1,11 @@
+public class RefrigeratorSensor {
+    private int externalTemp;
+
+    public RefrigeratorSensor(int externalTemp) {
+        this.externalTemp = externalTemp;
+    }
+
+    public int getExternalTemp() {
+        return externalTemp;
+    }
+}
